d5bd364f3485fcc9453e1a7bcb001cc06d25b79a
Oleksandr Kudinov <oleksandr.kudinov@eleks.com>
2017-01-18 15:01:05 +0200

    Added common abstract BaseMessage model;

---
+++ OChat/OChat.Common/BaseMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace OChat.Common
+{
+    public abstract class BaseMessage
+    {
+        public Int32 MessageId { get; set; }
+        public DateTime SendTimeUtc { get; set; }
+    }
+}
