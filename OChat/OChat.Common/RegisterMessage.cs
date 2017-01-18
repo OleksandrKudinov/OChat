125b1978d6b532add7b442beaa91ebc948e46d33
Oleksandr Kudinov <oleksandr.kudinov@eleks.com>
2017-01-18 19:11:48 +0200

    Added RegisterMessage type, for new user registration;

---
+++ OChat/OChat.Common/RegisterMessage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace OChat.Common
+{
+    public sealed class RegisterMessage
+    {
+        public String Login { get; set; }
+        public String Password { get; set; }
+    }
+}
