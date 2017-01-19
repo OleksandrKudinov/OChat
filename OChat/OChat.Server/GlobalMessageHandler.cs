167e1440241c4953bb7d64fa0e812fdde68cf148
Oleksandr Kudinov <oleksandr.kudinov@eleks.com>
2017-01-19 18:09:21 +0200

    Added GlobalMessageHandler type. Its responsibilities are: getting the from the IMessageHandlerProvider instance the implementation of IMessageHandler for current message type and passing message to this handler;

---
+++ OChat/OChat.Server/GlobalMessageHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using OChat.Common;
+
+namespace OChat.Server
+{
+    public sealed class GlobalMessageHandler : IMessageHandler<BaseMessage>
+    {
+        public GlobalMessageHandler(IMessageHandlerProvider messageHandlerProvider)
+        {
+            if (messageHandlerProvider == null)
+            {
+                throw new ArgumentNullException("Please, create and register implementation of IMessageHandlerProvider;");
+            }
+
+            _messageHandlerProvider = messageHandlerProvider;
+        }
+
+        public void Handle(BaseMessage message)
+        {
+            Boolean isResolved = false;
+
+            if (message is TextMessage)
+            {
+                ResolveHandlerAndProcess<TextMessage>(message, out isResolved);
+            }
+
+            if (message is RegisterMessage)
+            {
+                ResolveHandlerAndProcess<RegisterMessage>(message, out isResolved);
+            }
+
+            if (!isResolved)
+            {
+                throw new Exception($"Can't handle {message.GetType().Name}. IMessageHandler<{message.GetType().Name}> instance don't exist");
+            }
+        }
+
+        private void ResolveHandlerAndProcess<T>(BaseMessage message, out Boolean isResolved) where T : BaseMessage
+        {
+            IMessageHandler<T> handler = _messageHandlerProvider.GetMessageHandler<T>();
+            isResolved = handler != null;
+            handler?.Handle(message as T);
+        }
+
+        private readonly IMessageHandlerProvider _messageHandlerProvider;
+    }
+}
