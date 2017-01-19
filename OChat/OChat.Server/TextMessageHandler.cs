d58e99f5e353074b8ba5d599d52614095ff0da3a
Oleksandr Kudinov <oleksandr.kudinov@eleks.com>
2017-01-19 17:11:59 +0200

    Added TextMessageHandler type - implementation of IMessageHandler<TextMessage>

---
+++ OChat/OChat.Server/TextMessageHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OChat.Common;
+
+namespace OChat.Server
+{
+    public sealed class TextMessageHandler : IMessageHandler<TextMessage>
+    {
+        public TextMessageHandler()
+        {
+
+        }
+        public void Handle(TextMessage message)
+        {
+            Console.WriteLine(message.Text);
+        }
+    }
+}
