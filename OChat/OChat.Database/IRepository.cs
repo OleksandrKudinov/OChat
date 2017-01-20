025a18ed2c844d46263710dcf8955c47dc2a0ee0
Oleksandr Kudinov <oleksandr.kudinov@eleks.com>
2017-01-20 18:13:40 +0200

    Created IRepository<T> interface;

---
+++ OChat/OChat.Database/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OChat.Database
+{
+    public interface IRepository<T>
+    {
+        void Add(T entity);
+        void Remove(T entity);
+        T GetById(Object id);
+        IEnumerable<T> Find(Func<T, Boolean> predicate);
+    }
+}
