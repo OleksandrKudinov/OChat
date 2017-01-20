cd01274de81a2c27b2be9303feed3edc298c6b15
Oleksandr Kudinov <oleksandr.kudinov@eleks.com>
2017-01-20 18:20:39 +0200

    Added usage of TextMessageModelBuilder for TextMessage database model configuration;

--- OChat/OChat.Database/ApplicationDbContext.cs
+++ OChat/OChat.Database/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Entity;
+using OChat.Database.Configuration;
 using OChat.Common;
 
 namespace OChat.Database
@@ -8,6 +9,12 @@ namespace OChat.Database
         public ApplicationDbContext() : base("localSQLInstance")
         {
             System.Data.Entity.Database.SetInitializer(new DropCreateDatabaseIfModelChanges<ApplicationDbContext>());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            new TextMessageModelBuilder().ConfigureDbModel(modelBuilder);
+            base.OnModelCreating(modelBuilder);
         }
 
         public DbSet<TextMessage> TextMessages { get; set; }
