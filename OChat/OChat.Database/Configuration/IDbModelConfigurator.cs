0e49c458bc48df01d91006d39fc818e68b49d98b
Oleksandr Kudinov <oleksandr.kudinov@eleks.com>
2017-01-20 18:10:22 +0200

    Added IDbModelConfigurator interface for EF Fluent API database model configuration;

---
+++ OChat/OChat.Database/Configuration/IDbModelConfigurator.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace OChat.Database.Configuration
+{
+    internal interface IDbModelConfigurator<T>
+    {
+        void ConfigureDbModel(DbModelBuilder builder);
+    }
+}
