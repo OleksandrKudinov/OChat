e469fa37ae911375ed1af122070db336e3084e23
Oleksandr Kudinov <oleksandr.kudinov@eleks.com>
2017-01-20 16:42:45 +0200

    Created ApplicationDbContext;

---
+++ OChat/OChat.Database/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+namespace OChat.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("localSQLInstance")
+        {
+            System.Data.Entity.Database.SetInitializer(new DropCreateDatabaseIfModelChanges<ApplicationDbContext>());
+        }
+    }
+}
