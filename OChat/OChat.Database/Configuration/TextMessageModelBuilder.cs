6e44f4ffb08e34cb0479db9a03c0879268b4ddf2
Oleksandr Kudinov <oleksandr.kudinov@eleks.com>
2017-01-20 18:12:59 +0200

    Added TextMessageModelBuilder class for next EF Fluent API configuration;

---
+++ OChat/OChat.Database/Configuration/TextMessageModelBuilder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.Entity;
+using OChat.Common;
+
+
+namespace OChat.Database.Configuration
+{
+    internal class TextMessageModelBuilder : IDbModelConfigurator<TextMessage>
+    {
+        public void ConfigureDbModel(DbModelBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
