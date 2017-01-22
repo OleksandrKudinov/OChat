1a295611624c2cbd0ebb2407d0843c690f6d39fd
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-01-22 14:51:14 +0200

    Added AutofacConfigurator for loading referenced assemblies and register public interfaces;

---
+++ OChat/OChat.Server/AutofacConfigurator.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using OChat.Common;
+
+namespace OChat.Server
+{
+    public class AutofacConfigurator : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule<OChat.Database.AutofacConfigurator>();
+            builder.RegisterType<GlobalMessageHandler>().As<IMessageHandler<BaseMessage>>().SingleInstance();
+            builder.RegisterType<TextMessageHandler>().As<IMessageHandler<TextMessage>>().SingleInstance();
+        }
+    }
+}
