7780136a85a6974d6df867934b88534385e72181
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-01-22 21:14:37 +0200

    Created AccountRepository;

---
+++ OChat/OChat.Database/AccountRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using OChat.Common;
+
+namespace OChat.Database
+{
+    internal sealed class AccountRepository : IRepository<Account>
+    {
+        public void Add(Account entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException($"{nameof(entity)} is null");
+            }
+
+            using (var context = new ApplicationDbContext())
+            {
+                context.Accounts.Add(entity);
+                // TODO use async
+                context.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Account> Find(Func<Account, Boolean> predicate)
+        {
+            if (predicate == null)
+            {
+                throw new ArgumentNullException($"{nameof(predicate)} is null");
+            }
+
+            using (var context = new ApplicationDbContext())
+            {
+                IEnumerable<Account> result = context.Accounts.Where(predicate).ToArray();
+                return result;
+            }
+        }
+
+        public Account GetById(Object id)
+        {
+            String accountId = id as String;
+            if (String.IsNullOrWhiteSpace(accountId))
+            {
+                throw new ArgumentNullException($"{nameof(id)} is null");
+            }
+
+            using (var context = new ApplicationDbContext())
+            {
+                Account result = context.Accounts
+                    .FirstOrDefault(account => account.AccountId == accountId);
+                return result;
+            }
+        }
+
+        public void Remove(Account entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException($"{nameof(entity)} is null");
+            }
+
+            using (var context = new ApplicationDbContext())
+            {
+                context.Accounts.Remove(entity);
+                //TODO use async 
+                context.SaveChanges();
+            }
+        }
+    }
+}
