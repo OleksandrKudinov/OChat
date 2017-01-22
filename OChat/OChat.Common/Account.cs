a85c76589c6d7a79b426f9731cafa03a7266a6eb
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-01-22 20:50:00 +0200

    Changed AccountId from Int32 to String;

--- OChat/OChat.Common/Account.cs
+++ OChat/OChat.Common/Account.cs
@@ -4,7 +4,7 @@ namespace OChat.Common
 {
     public sealed class Account
     {
-        public Int32 AccountId { get; set; }
+        public String AccountId { get; set; }
         public String Login { get; set; }
         public String PasswordHash { get; set; }
         public String FirstName { get; set; }
