915b01eddcdb854e5e671ec2d7851dfe4f50b98b
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-01-22 14:44:31 +0200

    Make TextMessageRepository sealed;

--- OChat/OChat.Database/Configuration/TextMessageModelBuilder.cs
+++ OChat/OChat.Database/Configuration/TextMessageModelBuilder.cs
@@ -5,7 +5,7 @@ using OChat.Common;
 
 namespace OChat.Database.Configuration
 {
-    internal class TextMessageModelBuilder : IDbModelConfigurator<TextMessage>
+    internal sealed class TextMessageModelBuilder : IDbModelConfigurator<TextMessage>
     {
         public void ConfigureDbModel(DbModelBuilder builder)
         {
