ef827d1de41d5545a0accb94ac8927a987a2680f
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-01-23 23:12:03 +0200

    Added ContextRequest;

---
+++ OChat/OChat.Server/RequestContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OChat.Common;
+
+namespace OChat.Server
+{
+    internal sealed class RequestContext
+    {
+        public String SessionId { get; set; }
+        public Account Identity { get; set; }
+        public BaseMessage Message { get; set; }
+        public Result Result { get; set; }
+    }
+}
