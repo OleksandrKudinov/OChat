885d1c71eea3fde6f0680b4b675d8dbcb400c6f9
Oleksandr Kudinov <oleksandr.kudinov.1993@gmail.com>
2017-01-23 23:04:39 +0200

    Added IChannelProvider interface;

---
+++ OChat/OChat.Server/IChannelProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OChat.Server
+{
+    public interface IChannelProvider<T>
+    {
+        Boolean AddChannel(String identifier, IChannel<T> channel);
+        Boolean RemoveChannel(String identifier);
+        IChannel<T> GetChannel(String identifier);
+    }
+}
